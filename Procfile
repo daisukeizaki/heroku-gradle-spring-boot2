62156a47596281068093c72c33fbc3bdad2a0b29
daisukeizaki <daisuke.izaki@gmail.com>
2017-05-28 21:26:16 +0900

    init commit

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: java $JAVA_OPTS -Dserver.port=$PORT -jar target/gs-spring-boot-0.1.0.jar
+# web: java $JAVA_OPTS -Dserver.port=$PORT -jar app.jar
---
+++ src/main/java/hello/HelloController.java
@@ -0,0 +1,18 @@
+package hello;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+    @RequestMapping("/thankyou")
+    public String thankyougs() {
+        return "Thank you Spring Boot!";
+    }
+}
